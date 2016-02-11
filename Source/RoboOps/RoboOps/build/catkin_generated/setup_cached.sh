3b7332a09c5a33994e953af8580782be250b8c1b
Richard Yang <ruiqi888@gmail.com>
2016-02-10 18:02:20 -0700

    Servo publisher package

---
+++ Source/RoboOps/RoboOps/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/richard/ros_pkg/RoboOps/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/richard/ros_pkg/RoboOps/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/richard/ros_pkg/RoboOps/build/devel/lib:/home/richard/ros_pkg/RoboOps/build/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/richard/ros_pkg/RoboOps/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/richard/ros_pkg/RoboOps/build/devel/lib/pkgconfig:/home/richard/ros_pkg/RoboOps/build/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PYTHONPATH="/home/richard/ros_pkg/RoboOps/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/richard/ros_pkg/RoboOps/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/richard/ros_pkg/RoboOps:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
