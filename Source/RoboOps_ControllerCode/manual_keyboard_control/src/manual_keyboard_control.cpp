8e7758b61efcc4ceda3fcc32508d273b283aeb01
jrprj <jr.petrutiu@gmail.com>
2016-04-02 21:27:28 -0600

    WIP updating manual commands

--- Source/RoboOps_ControllerCode/manual_keyboard_control/src/manual_keyboard_control.cpp
+++ Source/RoboOps_ControllerCode/manual_keyboard_control/src/manual_keyboard_control.cpp
@@ -15,27 +15,30 @@ Commands:
 */
 
 /* Servo and DC Motor reference
-+--------+-----+-------+-------+---------------------------+
-| `-..-` ‖ I2C |  MSG  | ARRAY | DESCRIPTION               |
-| .-``-. ‖ PIN | INDEX | INDEX |                           |
-+========+=====+=======+=======+===========================+
-|        ‖  0  |   0   |   0   | Arm Base                  |
-|  Arm   ‖  1  |   1   |   1   | Arm Shoulder              |
-| Servos ‖  2  |   2   |   2   | Arm Elbow                 |
-|        ‖  3  |   3   |   3   | Arm Wrist                 |
-+--------+-----+-------+-------+---------------------------+
-|  Drive ‖  4  |   0   |   0   | Back Wheel                |
-| Servos ‖  5  |   1   |   1   | Front Right Wheel         |
-|        ‖  6  |   2   |   2   | Front Left Wheel          |
-+--------+-----+-------+-------+---------------------------+
-|  Mast  ‖  8  |   0   |   0   | Mast Servo                |
-+--------+-----+-------+-------+---------------------------+
-|        ‖ 11  |   0   |   0   | Rear Wheel                |
-|  Drive ‖ 12  |   1   |   1   | Side Right Wheels (BOTH)  |
-|   DC   ‖ 13  |   2   |   2   | Side Left Wheels (BOTH)   |
-| Motors ‖ 14  |   3   |   3   | Front Right Wheel         |
-|        ‖ 15  |   4   |   4   | Front Left Wheels         |
-+--------+-----+-------+-------+---------------------------+  */
++---------+-------+---------------------------+
+| `-..-`  ‖  MSG  | DESCRIPTION               |
+| .-``-.  ‖ INDEX |                           |
++=========+=======+===========================+
+|         ‖   0   | Arm Base                  |
+|  Arm    ‖   1   | Arm Shoulder              |
+| Servos  ‖   2   | Arm Elbow                 |
+|         ‖   3   | Arm Wrist                 |
++---------+-------+---------------------------+
+| Gripper ‖   4   | Gripper vacuum            |
++---------+-------+---------------------------+
+|  Drive  ‖   0   | Back Wheel                |
+| Servos  ‖   1   | Front Right Wheel         |
+|         ‖   2   | Front Left Wheel          |
++---------+-------+---------------------------+
+|  Mast   ‖   0   | Mast Servo                |
++---------+-------+---------------------------+
+|         ‖   0   | Rear Wheel                |
+|  Drive  ‖   1   | Side Right Wheels (BOTH)  |
+|   DC    ‖   2   | Side Left Wheels (BOTH)   |
+| Motors  ‖   3   | Front Right Wheel         |
+|         ‖   4   | Front Left Wheels         |
++---------+-------+---------------------------+   */
+
 // Camera mast
 #define MAST 0
 // Arm servos
@@ -43,6 +46,8 @@ Commands:
 #define ARM_SHOULDER 1
 #define ARM_ELBOW 2
 #define ARM_WRIST 3
+// Arm gripper
+#define ARM_GRIPPER 4
 // Steering servos
 #define STEER_BACK 0
 #define STEER_FRONT_RIGHT 1
@@ -71,6 +76,7 @@ std_msgs::Int16MultiArray arm_servo_message;
 
 // Steer servo array
 int16_t steer_servo[3];
+std_msgs::Int16MultiArray steer_servo_message;
 
 // Drive motor array
 int16_t drive_motors[8];
@@ -97,10 +103,19 @@ void keyUp(const keyboard::Key& key) {
 /***** publish_arm_servo_update() ***
     Publishes a new angle for the specified arm servo.
     @INPUT int servo_index - index of servo to be published     */
-void publish_arm_servo_update(int servo_index) {
-    arm_servo_message.data[0] = servo_index;
-    arm_servo_message.data[1] = arm_servo[servo_index];
+void publish_arm_servo_update() {
+    arm_servo_message[0] = arm_servo[0];
+    arm_servo_message[1] = arm_servo[1];
+    arm_servo_message[2] = arm_servo[2];
+    arm_servo_message[3] = arm_servo[3];
     arm_cmd_manual->publish(arm_servo_message);
+}
+
+void publish_steer_servo_update() {
+    steer_servo_message.data[0] = steer_servo[0];
+    steer_servo_message.data[1] = steer_servo[1];
+    steer_servo_message.data[2] = steer_servo[2];
+    steer_cmd_manual->publish(steer_servo_message);
 }
 
 /***** publish_drive_motor_update() ***
@@ -112,9 +127,6 @@ void publish_drive_motor_update() {
     drive_motor_message.data[2] = drive_motors[2];
     drive_motor_message.data[3] = drive_motors[3];
     drive_motor_message.data[4] = drive_motors[4];
-    drive_motor_message.data[5] = steer_servo[0];
-    drive_motor_message.data[6] = steer_servo[1];
-    drive_motor_message.data[7] = steer_servo[2];
     drive_cmd_manual->publish(drive_motor_message);
 }
 
@@ -122,32 +134,29 @@ void publish_drive_motor_update() {
     Initialize all message and servo arrays */
 void initialize_servos() {
 
-	// Initialize arm servo update message array
-    arm_servo_message.data.clear();
-    arm_servo_message.data.push_back(0);
-    arm_servo_message.data.push_back(0);
-
     // Initialize arm servo array
     arm_servo[0] = 0;
     arm_servo[1] = 0;
     arm_servo[2] = 0;
     arm_servo[3] = 0;
 
-    // Initialize drive motor update message array
-    drive_motor_message.data.clear();
-    drive_motor_message.data.push_back(0);
-    drive_motor_message.data.push_back(0);
-    drive_motor_message.data.push_back(0);
-    drive_motor_message.data.push_back(0);
-    drive_motor_message.data.push_back(0);
-    drive_motor_message.data.push_back(0);
-    drive_motor_message.data.push_back(0);
-    drive_motor_message.data.push_back(0);
+	// Initialize arm servo update message array
+    arm_servo_message.data.clear();
+    arm_servo_message.data.push_back(0);
+    arm_servo_message.data.push_back(0);
+    arm_servo_message.data.push_back(0);
+    arm_servo_message.data.push_back(0);
 
     // Initialize steer servo array
     steer_servo[0] = 0;
     steer_servo[1] = 0;
     steer_servo[2] = 0;
+
+    // Initialize steer servo update message array
+    drive_motor_message.data.clear();
+    drive_motor_message.data.push_back(0);
+    drive_motor_message.data.push_back(0);
+    drive_motor_message.data.push_back(0);
 
     // Initialize drive motor array
     drive_motors[0] = 0;
@@ -156,15 +165,13 @@ void initialize_servos() {
     drive_motors[3] = 0;
     drive_motors[4] = 0;
 
-    // Put all arm servos at 0 degrees
-    for (int i = 0; i < 4; i++) {
-        publish_arm_servo_update(i);
-    }
-
-
-		// Set all drive motor speeds to 0
-    publish_drive_motor_update();
-
+    // Initialize drive motor update message array
+    drive_motor_message.data.clear();
+    drive_motor_message.data.push_back(0);
+    drive_motor_message.data.push_back(0);
+    drive_motor_message.data.push_back(0);
+    drive_motor_message.data.push_back(0);
+    drive_motor_message.data.push_back(0);
 }
 
 /***** initialize_key_states() ***
@@ -194,25 +201,27 @@ void initialize_key_states() {
 
 int main(int argc, char **argv) {
     // Initialize ROS elements
-    ros::init(argc, argv, "continuous_control");
-    ros::NodeHandle n;
+    ros::init(argc, argv, "manual_keyboard_control");
+    ros::NodeHandle n;  
     ros::Rate loop_rate(20);
 
     // Publishers to motor controller
-		arm_cmd_manual = new ros::Publisher();
-		drive_cmd_manual = new ros::Publisher();
+	arm_cmd_manual = new ros::Publisher();
+    steer_cmd_manual = new ros::Publisher();
+	drive_cmd_manual = new ros::Publisher();
 
     *arm_cmd_manual = n.advertise<std_msgs::Int16MultiArray>("arm_cmd_manual", 1000);
+    *steer_cmd_manual = n.advertise<std_msgs::Int16MultiArray>("steer_cmd_manual", 1000);
     *drive_cmd_manual = n.advertise<std_msgs::Int16MultiArray>("drive_cmd_manual", 1000);
     
     // Keyboard subscribers
     ros::Subscriber keydown = n.subscribe("keyboard/keydown", 10, keyDown);
     ros::Subscriber keyup = n.subscribe("keyboard/keyup", 10, keyUp);
 
-    std::cout << "STARTED PWM PUBLISHER!!!" << std::endl;
-
     initialize_servos();      
     initialize_key_states();
+
+    std::cout << "STARTED PWM PUBLISHER!!!" << std::endl;
 
     while (ros::ok())
     {
@@ -221,20 +230,36 @@ int main(int argc, char **argv) {
             int target_angle = 0;
             int angle_delta = 0;
 
+            // For each arm servo
             for (int i = 0; i < 4; i++) {
-                if (arm_servo[1] > 55 && arm_servo[3] != -40 && i != 3) {
+                // If the shoulder is extended and the wrist is not brought in
+                // then bring in the wrist (to reduce strain on shoulder servo)
+                //      Which means if the current servo is not the wrist, skip it
+                //      unless the wrist is retracted!
+                if (arm_servo[ARM_SHOULDER] > 55 && arm_servo[ARM_WRIST] != -40 && i != ARM_WRIST) {
                     continue;
                 }
 
-                if (i == 0 && arm_servo[0] != 0 && arm_servo[1] != 30) {
-                    target_angle = arm_servo[0];
-                } else if (i == 1 && arm_servo[0] != 0) {
+                if (i == ARM_BASE && arm_servo[ARM_BASE] != 0 && arm_servo[ARM_SHOULDER] == 30) {
+                    // If current servo is the base, and the base is not at 0 degrees,
+                    // and the should is currently at 30 degrees (to avoid hitting things)
+                    // then set the base target angle to 0 degrees
+                    target_angle = 0;
+                } else if (i == ARM_SHOULDER && arm_servo[ARM_BASE] != 0) {
+                    // If the current servo is the shoulder, and the base is not 0 degrees,
+                    // set the shoulder target angle to 30 degrees.
                     target_angle = 30;
-                } else if (i == 2 && (arm_servo[0] != 0 || arm_servo[1] > 20)) {
-                    target_angle = arm_servo[1] + 30;
-                } else if (i == 3 && arm_servo[1] > 50) {
+                } else if (i == ARM_ELBOW && (arm_servo[ARM_BASE] != 0 || arm_servo[ARM_SHOULDER] > 20)) {
+                    // If the current servo is the elbow, and either the base is not at 0 or the
+                    // shoulder is bigger than 20 degrees, set the target angle for the elbow 
+                    // to whatever the shoulder is plus 30 degrees (to avoid collisions)
+                    target_angle = arm_servo[ARM_ELBOW] + 30;
+                } else if (i == ARM_WRIST && arm_servo[ARM_SHOULDER] > 50) {
+                    // If the current servo is the wrist, and the shoulder is at more than
+                    // 50 degrees, set the wrist angle to -40
                     target_angle = -40;
                 } else {
+                    // Otherwise set the target angle to "home" (ie 0 degrees)
                     target_angle = 0;
                 }
 
@@ -256,44 +281,44 @@ int main(int argc, char **argv) {
 
                 // Publish change in angle
                 arm_servo[i] += angle_delta;
-                publish_arm_servo_update(i);
             }
+            publish_arm_servo_update();
         }
 
         // Arm base (m and n)
         if (keys[keyboard::Key::KEY_n]) {
             arm_servo[ARM_BASE] += 1;
-            publish_arm_servo_update(ARM_BASE);
+            publish_arm_servo_update();
         } else if (keys[keyboard::Key::KEY_m]) {
             arm_servo[ARM_BASE] -= 1;
-            publish_arm_servo_update(ARM_BASE);
+            publish_arm_servo_update();
         }
 
         // Arm shoulder (j and u)
         if (keys[keyboard::Key::KEY_j]) {
             arm_servo[ARM_SHOULDER] += 1;
-            publish_arm_servo_update(ARM_SHOULDER);
+            publish_arm_servo_update();
         } else if (keys[keyboard::Key::KEY_u]) {
             arm_servo[ARM_SHOULDER] -= 1;
-            publish_arm_servo_update(ARM_SHOULDER);
+            publish_arm_servo_update();
         }
 
         // Arm elbow (i and k)
         if (keys[keyboard::Key::KEY_i]) {
             arm_servo[ARM_ELBOW] += 1;
-            publish_arm_servo_update(ARM_ELBOW);
+            publish_arm_servo_update();
         } else if (keys[keyboard::Key::KEY_k]) {
             arm_servo[ARM_ELBOW] -= 1;
-            publish_arm_servo_update(ARM_ELBOW);
+            publish_arm_servo_update();
         }
 
         // Arm wrist (o and l)
         if (keys[keyboard::Key::KEY_o]) {
             arm_servo[ARM_WRIST] += 1;
-            publish_arm_servo_update(ARM_WRIST);
+            publish_arm_servo_update();
         } else if (keys[keyboard::Key::KEY_l]) {
             arm_servo[ARM_WRIST] -= 1;
-            publish_arm_servo_update(ARM_WRIST);
+            publish_arm_servo_update();
         }
         
         // Arm gripper (b)
@@ -311,23 +336,22 @@ int main(int argc, char **argv) {
             steer_servo[STEER_BACK] += 3;
             steer_servo[STEER_FRONT_RIGHT] += 3;
             steer_servo[STEER_FRONT_LEFT] += 3;
-            publish_drive_motor_update();
+            publish_steer_servo_update();
         } else if (keys[keyboard::Key::KEY_d]) {
             steer_servo[STEER_BACK] -= 3;
             steer_servo[STEER_FRONT_RIGHT] -= 3;
             steer_servo[STEER_FRONT_LEFT] -= 3;
-            publish_drive_motor_update();
+            publish_steer_servo_update();
         }
 	    if (keys[keyboard::Key::KEY_f]) {
             steer_servo[STEER_BACK] = 0;
             steer_servo[STEER_FRONT_RIGHT] = 0;
             steer_servo[STEER_FRONT_LEFT] = 0;
-            publish_drive_motor_update();
+            publish_steer_servo_update();
         }
 
         // Drive motors (w and s and x)
         if (keys[keyboard::Key::KEY_w]) {
-
             (drive_motors[DRIVE_REAR] < 2000) ? drive_motors[DRIVE_REAR] += 100 : drive_motors[DRIVE_REAR] = 2000;
             (drive_motors[DRIVE_SIDE_RIGHT] < 2000) ? drive_motors[DRIVE_SIDE_RIGHT] += 100 : drive_motors[DRIVE_SIDE_RIGHT] = 2000;
             (drive_motors[DRIVE_SIDE_LEFT] < 2000) ? drive_motors[DRIVE_SIDE_LEFT] += 100 : drive_motors[DRIVE_SIDE_LEFT] = 2000;
