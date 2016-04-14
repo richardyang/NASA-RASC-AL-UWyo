22ec6f0fc0a49bbe86ee3c1faa7cd21a40dba922
jrprj <jr.petrutiu@gmail.com>
2016-04-14 10:29:19 -0600

    Added protothread Libraries to 'Source/Arduino/libraries'

--- Source/RoboOps_ControllerCode/manual_keyboard_control/build/catkin_generated/setup_cached.sh
+++ Source/RoboOps_ControllerCode/manual_keyboard_control/build/catkin_generated/setup_cached.sh
@@ -9,8 +9,8 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/jr/github/RoboOps-Senior-Design/Source/RoboOps_ControllerCode/manual_keyboard_control/build/devel:$CMAKE_PREFIX_PATH"
 export CPATH="/home/jr/github/RoboOps-Senior-Design/Source/RoboOps_ControllerCode/manual_keyboard_control/build/devel/include:$CPATH"
-export LD_LIBRARY_PATH="/home/jr/github/RoboOps-Senior-Design/Source/RoboOps_ControllerCode/manual_keyboard_control/build/devel/lib:/home/jr/github/RoboOps-Senior-Design/Source/RoboOps_ControllerCode/manual_keyboard_control/build/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib:/home/jr/torch/install/lib:/home/jr/torch/install/lib"
-export PATH="/home/jr/github/RoboOps-Senior-Design/Source/RoboOps_ControllerCode/manual_keyboard_control/build/devel/bin:$PATH"
+export LD_LIBRARY_PATH="/home/jr/github/RoboOps-Senior-Design/Source/RoboOps_ControllerCode/manual_keyboard_control/build/devel/lib:/home/jr/github/RoboOps-Senior-Design/Source/RoboOps_ControllerCode/manual_keyboard_control/build/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/jr/github/RoboOps-Senior-Design/Source/RoboOps_ControllerCode/manual_keyboard_control/build/devel/bin:/opt/ros/indigo/bin:/home/jr/.opam/system/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/jr/Applications/android-studio/bin"
 export PKG_CONFIG_PATH="/home/jr/github/RoboOps-Senior-Design/Source/RoboOps_ControllerCode/manual_keyboard_control/build/devel/lib/pkgconfig:/home/jr/github/RoboOps-Senior-Design/Source/RoboOps_ControllerCode/manual_keyboard_control/build/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PYTHONPATH="/home/jr/github/RoboOps-Senior-Design/Source/RoboOps_ControllerCode/manual_keyboard_control/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/jr/github/RoboOps-Senior-Design/Source/RoboOps_ControllerCode/manual_keyboard_control/build/devel/share/common-lisp"
